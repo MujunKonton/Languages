7039867e5b5831b6c9ef4b2444c0035d927bcde0
矛盾と混沌 <144119243+MujunKonton@users.noreply.github.com>
2024-11-06 21:57:30 +0900

    Add files via upload

---
+++ words.js
@@ -0,0 +1,863 @@
+var words=`*나	na	私
+우리	uɾi	私たち
+너	nɔ	あなた
+너희들	nɔhidɯl	あなたたち
+그	kɯ	彼
+그녀	kɯɲɔ	彼女
+그들	kɯdɯl	彼等
+이것	iɡɔt	これ
+그것	kɯɡɔt	それ
+저것	tʃɔɡɔt	あれ
+여기	jɔɡi	ここ
+거기	kɔɡi	そこ
+저기	tʃɔɡi	あそこ
+누구	nuɡu	誰
+언제	ɔndʒe	いつ
+어디	ɔdi	どこ
+무엇	muɔt	なに
+어떻게	ɔtɔkke	どうやって
+왜	wɛ	なぜ
+아침	atʃʰim	朝
+낮	natʃ	昼
+밤	pam	夜
+하늘	hanɯl	空
+태양	tʰɛjaŋ	太陽
+달	tal	月
+바람	paɾam	風
+산	san	山
+강	kaŋ	川
+바다	paɾa	海
+아버지	abɔdʒi	父
+어머니	ɔmɔni	母
+형제	hjɔŋdʒe	兄弟
+자매	tʃame	姉妹
+가족	kadʒɔk	家族
+마을	maɯl	町
+집	tʃip	家
+가게	kaɡe	店
+역	jɔk	駅
+회사	hwɛsa	会社
+학교	hakjo	学校
+도서관	tɔʃɔɡwan	図書館
+슈퍼마켓	sjupʰɔmakʰet	スーパーマーケット
+맥도날드	mɛktonaltɯ	マクドナルド
+지하철	tʃihatʃʰɔl	電車
+자동차	tʃadonʧʰa	自動車
+버스	pɔsɯ	バス
+도로	toɾo	道路
+고양이	kojaŋi	猫
+개	ke	犬
+새	sɛ	鳥
+물고기	muɭkoɡi	魚
+나무	nɔmu	木
+풀	pʰuɭ	草
+꽃	kotʃʰ	花
+머리	mɔɾi	頭
+얼굴	ɔlɡuɭ	顔
+목	mok	首
+가슴	kasɯm	胸
+배	pɛ	腹
+등	tɯŋ	背中
+팔	pʰaɭ	腕
+다리	taɾi	足
+마음	maɯm	心
+시간	siɡan	時間
+말	maɭ	言葉
+스마트폰	sɯmatʰɯpʰon	スマホ
+시계	siɡje	時計
+책	tʃʰɛk	本
+가방	kapan	鞄
+배낭	pɛnaŋ	リュックサック
+신발	sinpaɭ	靴
+양말	jaŋmaɭ	靴下
+옷	otʃ	服
+음식	ɯmsik	食べ物
+음료수	ɯmnʲosu	飲み物
+지갑	tʃiɡap	財布
+돈	ton	お金
+전등	tʃɔndɯŋ	電灯
+책상	tʃʰɛksaŋ	机
+의자	ɯidʒa	椅子
+문	mun	扉
+침대	tʃʰimdɛ	ベッド
+옷장	ottʃaŋ	クローゼット
+벽	pjɔk	壁
+오늘	ɔnɯl	今日
+어제	ɔdʒɛ	昨日
+내일	nɛiɭ	明日
+지금	tʃiɡɯm	今
+있다	itta	ある
+먹다	mɔk̚da	食べる
+마시다	masida	飲む
+자다	tʃada	寝る
+일어나다	iɾɔnada	起きる
+앉다	anta	座る
+서다	sɔda	立つ
+걷다	kɔta	歩く
+쓰다	sɯda	使う
+살다	saɭda	生きる
+살다	saɭda	住む
+알다	alda	知る
+생각하다	seŋɡakʰada	考える
+보다	pʰoda	見る
+듣다	tɯk̚da	聞く
+말하다	maɾhada	話す
+보여주다	podʒɾuda	見せる
+공부하다	koŋbuhada	勉強する
+좋다	tʃɔda	良い
+나쁘다	nappɯda	悪い
+크다	kɯda	大きい
+작다	tʃak̚da	小さい
+어렵다	ɔɾjɔp̚da	難しい
+쉽다	swip̚da	簡単な
+덥다	tɔp̚da	暑い
+춥다	tʃup̚da	寒い
+밝다	paɭkta	明るい
+어둡다	ɔdup̚da	暗い
+넓다	nɔɭpʰda	広い
+좁다	tʃop̚da	狭い
+굵다	kuɭkta	太い
+가늘다	kanɯɭda	細い
+빨갛다	ppaɭɡada	赤い
+파랗다	pʰaɾada	青い
+초록색	tʃʰoɾoksek	緑の
+노랗다	noɾatta	黄色い
+까맣다	kkamatta	黒い
+하얗다	hajatta	白い
+안녕하세요	annjɔŋɦasɛjo	こんにちは
+안녕히 가세요	annjɔŋɦiɡasejo	さようなら
+감사합니다	kamsamida	ありがとう
+죄송합니다	tʃwesoŋhamnida	ごめんなさい
+부탁합니다	putʰakʰamnida	おねがいします
+모르겠어요	molɯgessɔjo	わかりません
+動詞고 싶다	～ko sipta	～したい
+動詞ㄹ 수 있다	～ɭ su itta	～できる
+안 動詞	an ～	～しない
+그리고	kɯrigo	そして
+네	ne	はい
+아니	ani	いいえ
+그러나	kɯrɔna	しかし
+～어서/아서/니까	ɔsɔ/asɔ/nikka	～なので
+그러니까	kɯrɔnikka	なので
+일본	ilbon	日本
+한국	hanguk	韓国
+*我	wǒ	私
+我们	wǒmen	私たち
+你	nǐ	あなた
+你们	nǐmen	あなたたち
+他	tā	彼
+她	tā	彼女
+他们	tāmen	彼等
+这	zhè	これ
+那	nà	それ,あれ
+这里	zhèlǐ	ここ
+那里	nàlǐ	そこ
+那里	nàlǐ	あそこ
+谁	shéi	誰
+什么时候	shénme shíhou	いつ
+哪里	nǎlǐ	どこ
+什么	shénme	なに
+怎么	zěnme	どうやって
+为什么	wèishénme	なぜ
+早晨	zǎochén	朝
+中午	zhōngwǔ	昼
+夜晚	yèwǎn	夜
+天空	tiānkōng	空
+太阳	tàiyáng	太陽
+月亮	yuèliàng	月
+风	fēng	風
+山	shān	山
+河	hé	川
+海	hǎi	海
+父亲	fùqīn	父
+母亲	mǔqīn	母
+兄弟	xiōngdì	兄弟
+姐妹	jiěmèi	姉妹
+家庭	jiātíng	家族
+城镇	chéngzhèn	町
+家	jiā	家
+店	diàn	店
+车站	chēzhàn	駅
+公司	gōngsī	会社
+学校	xuéxiào	学校
+图书馆	túshūguǎn	図書館
+超市	chāoshì	スーパーマーケット
+麦当劳	màidāngláo	マクドナルド
+电车	diànchē	電車
+汽车	qìchē	自動車
+公交车	gōngjiāochē	バス
+道路	dàolù	道路
+猫	māo	猫
+狗	gǒu	犬
+鸟	niǎo	鳥
+鱼	yú	魚
+树	shù	木
+草	cǎo	草
+花	huā	花
+头	tóu	頭
+脸	liǎn	顔
+颈	jǐng	首
+胸	xiōng	胸
+肚子	dùzi	腹
+背	bèi	背中
+手臂	shǒubì	腕
+脚	jiǎo	足
+心	xīn	心
+时间	shíjiān	時間
+词语	cíyǔ	言葉
+手机	shǒujī	スマホ
+手表	shǒubiǎo	時計
+书	shū	本
+包	bāo	鞄
+背包	bēibāo	リュックサック
+鞋	xié	靴
+袜子	wàzi	靴下
+衣服	yīfu	服
+食物	shíwù	食べ物
+饮料	yǐnliào	飲み物
+钱包	qiánbāo	財布
+钱	qián	お金
+灯	dēng	電灯
+桌子	zhuōzi	机
+椅子	yǐzi	椅子
+门	mén	扉
+床	chuáng	ベッド
+衣橱	yīchú	クローゼット
+墙	qiáng	壁
+今天	jīntiān	今日
+昨天	zuótiān	昨日
+明天	míngtiān	明日
+现在	xiànzài	今
+有	yǒu	ある
+吃	chī	食べる
+喝	hē	飲む
+睡觉	shuìjiào	寝る
+起床	qǐchuáng	起きる
+坐	zuò	座る
+站	zhàn	立つ
+走	zǒu	歩く
+用	yòng	使う
+活	huó	生きる
+住	zhù	住む
+知道	zhīdào	知る
+思考	sīkǎo	考える
+看	kàn	見る
+听	tīng	聞く
+说话	shuōhuà	話す
+给…看	gěi... kàn	見せる
+好	hǎo	良い
+坏	huài	悪い
+大	dà	大きい
+小	xiǎo	小さい
+难	nán	難しい
+简单	jiǎndān	簡単な
+热	rè	暑い
+冷	lěng	寒い
+明亮	míngliàng	明るい
+黑暗	hēi'àn	暗い
+宽	kuān	広い
+窄	zhǎi	狭い
+粗	cū	太い
+细	xì	細い
+红	hóng	赤い
+蓝	lán	青い
+绿色	lǜsè	緑の
+黄色	huángsè	黄色い
+黑	hēi	黒い
+白	bái	白い
+你好	nǐ hǎo	こんにちは
+再见	zàijiàn	さようなら
+谢谢	xièxiè	ありがとう
+对不起	duìbuqǐ	ごめんなさい
+拜托	bàituō	おねがいします
+能～	néng	能力がある
+会～	huì	～できる
+是		shì はい
+不是	bùshì	いいえ
+没有	méiyǒu	ない
+因为	yīnwèi	なぜなら
+但是	dànshì	しかし
+和	hé	～と
+日本	rìběn	日本
+中国	zhōngguó	中国
+听不懂	tīngbudǒng	(聞いても)わかりません。
+我不知道	wǒbùzhīdào	知りません。
+学习	xuéxí	勉強する
+*saya		私(丁寧)
+aku		私(カジュアル)
+kami		私たち(自分&その他)
+kita		私たち(自分&相手)
+kamu		きみ
+Anda		あなた
+kalian		あなたたち
+dia		彼
+dia		彼女
+mereka		彼等
+ini		これ
+itu		それ,あれ
+sini		ここ
+situ		そこ
+sana		あそこ
+siapa		誰
+kapan		いつ
+di mana		どこ
+apa		なに
+bagaimana		どうやって
+kenapa / mengapa		なぜ
+pagi		朝
+siang		昼
+malam		夜
+langit		空
+matahari		太陽
+bulan		月
+angin		風
+gunung		山
+sungai		川
+laut		海
+ayah		父
+ibu		母
+saudara laki-laki		兄弟
+saudara perempuan		姉妹
+keluarga		家族
+kota		町
+rumah		家
+toko		店
+stasiun		駅
+perusahaan		会社
+sekolah		学校
+perpustakaan		図書館
+supermarket		スーパーマーケット
+McDonald's		マクドナルド
+kereta api		電車
+mobil		自動車
+bus		バス
+jalan		道路
+kucing		猫
+anjing		犬
+burung		鳥
+ikan		魚
+pohon		木
+rumput		草
+bunga		花
+kepala		頭
+wajah		顔
+leher		首
+dada		胸
+perut		腹
+punggung		背中
+lengan		腕
+kaki		足
+hati		心
+waktu		時間
+kata-kata		言葉
+ponsel pintar		スマホ
+jam tangan		時計
+buku		本
+tas		鞄
+ransel		リュックサック
+sepatu		靴
+kaus kaki		靴下
+pakaian		服
+makanan		食べ物
+minuman		飲み物
+dompet		財布
+uang		お金
+lampu		電灯
+meja		机
+kursi		椅子
+pintu		扉
+tempat tidur		ベッド
+lemari pakaian		クローゼット
+dinding		壁
+hari ini		今日
+kemarin		昨日
+besok		明日
+sekarang		今
+ada		ある
+makan		食べる
+minum		飲む
+tidur		寝る
+bangun		起きる
+duduk		座る
+berdiri		立つ
+berjalan		歩く
+menggunakan		使う
+hidup		生きる
+tinggal		住む
+tahu		知る
+berpikir		考える
+melihat		見る
+mendengar		聞く
+berbicara		話す
+menunjukkan		見せる
+baik		良い
+buruk		悪い
+besar		大きい
+kecil		小さい
+sulit		難しい
+mudah		簡単な
+panas		暑い
+dingin		寒い
+terang		明るい
+gelap		暗い
+luas		広い
+sempit		狭い
+tebal		太い
+tipis		細い
+merah		赤い
+biru		青い
+hijau		緑の
+kuning		黄色い
+hitam		黒い
+putih		白い
+halo		こんにちは
+selamat tinggal		さようなら
+terima kasih		ありがとう
+maaf		ごめんなさい
+tolong		おねがいします
+Saya tidak mengerti	わかりません
+bisa		～できる
+ingin		～したい
+sudah		すでに～した
+belum		まだ～ない
+tidak		～しない
+bukan		～でない
+ya		はい
+dan		～と,そして
+tetapi		しかし
+karena/maka		だから
+Jepang	日本*
+ខ្ញុំ	kʰɲom	私
+យើង	jiəŋ	私たち
+អ្នក	ʔneak	あなた
+ពួកអ្នក	puək ʔneak	あなたたち
+គាត់	koat	彼
+នាង	niəŋ	彼女
+ពួកគាត់	puək koat	彼等
+នេះ	nih	これ
+នោះ	noh	それ
+នោះ	noh	あれ
+ទីនេះ	tiʔ nih	ここ
+ទីនោះ	tiʔ noh	そこ
+ទីនោះ	tiʔ noh	あそこ
+អ្នកណា	ʔneak naa	誰
+ពេលណា	peel naa	いつ
+កន្លែងណា	kɑnlaɛŋ naa	どこ
+អ្វី	ʔvey	なに
+ដោយរបៀបណា	daoy rɔbiəp naa	どうやって
+ហេតុអ្វី	haetʔvey	なぜ
+ព្រឹក	prɨk	朝
+ថ្ងៃត្រង់	tʰŋay trɑŋ	昼
+យប់	jup	夜
+មេឃ	miək	空
+ព្រះអាទិត្យ	prɨəh ʔatɨt	太陽
+ព្រះច័ន្ទ	prɨəh cɑn	月
+ខ្យល់	kʰʔcɑl	風
+ភ្នំ	pʰnom	山
+ទន្លេ	tonleʔ	川
+សមុទ្រ	sɑmut	海
+ឪពុក	ʔou puʔ	父
+ម្តាយ	mday	母
+បងប្អូន	baŋ pʔaun	兄弟
+បងប្អូនស្រី	baŋ pʔaun srei	姉妹
+គ្រួសារ	kruəsa	家族
+មិត្តភក្ដិ	mɨt pʰɨʔk	友達
+គ្រូ	kruː	先生
+សិស្ស	sɨʔs	生徒
+ទីក្រុង	tiʔ krɔŋ	町
+ផ្ទះ	pʰteah	家
+ហាង	haŋ	店
+ស្ថានីយ	stʰanii	駅
+ក្រុមហ៊ុន	kʔrɔm hɨn	会社
+សាលារៀន	saːla rian	学校
+រោងចក្រ	roŋ cak	工場
+បណ្ណាល័យ	pɑnnaʔlai	図書館
+ផ្សារទំនើប	psaʔ tumnɨp	スーパーマーケット
+ម៉ាក់ដូណាល់	makʔdonɑːl	マクドナルド
+រថភ្លើង	rut pʰləəŋ	電車
+ឡាន	laːn	自動車
+ឡានក្រុង	laːn krɔŋ	バス
+ផ្លូវ	pʰləv	道路
+ឆ្មា	cʰmaː	猫
+ឆ្កែ	cʰkae	犬
+បក្សី	pɑksei	鳥
+ត្រី	trəy	魚
+ដើមឈើ	daom cʰəə	木
+ស្មៅ	smao	草
+ផ្កា	pʰkaː	花
+ក្បាល	kbaal	頭
+មុខ	mokʰ	顔
+ក	kɑ	首
+ទ្រូង	truəŋ	胸
+ពោះ	puəh	腹
+ខ្នង	kʰnɑŋ	背中
+ដៃ	dai	腕
+ជើង	cəəŋ	足
+បេះដូង	beʔdoːŋ	心
+ពេលវេលា	peəl veəlaa	時間
+ពាក្យ	piək	言葉
+ទូរស័ព្ទដៃ	turasaʔ dai	スマホ
+នាឡិកា	niəlekaː	時計
+សៀវភៅ	siəvpʰəu	本
+កាបូប	kaːboːp	鞄
+កាបូបស្ពាយ	kaːboːp spaj	リュックサック
+ស្បែកជើង	sbaek cəəŋ	靴
+ស្រោមជើង	sraom cəəŋ	靴下
+សម្លៀកបំពាក់	sɑmliək bɑmpɛəʔ	服
+អាហារ	ʔaːhaa	食べ物
+ភេសជ្ជៈ	pʰeːsehcaʔ	飲み物
+កាបូបលុយ	kaːboːp luj	財布
+លុយ	luj	お金
+អំពូលអគ្គិសនី	ʔɑmpuəl ʔakkiʔsaʔniʔ	電灯
+តុ	tokʰ	机
+កៅអី	kʔaʊ ʔəy	椅子
+ទ្វារ	tvijə	扉
+គ្រែ	kreː	ベッド
+ទូរទុកខោអាវ	tur tukʰ kao aʋ	クローゼット
+ជញ្ជាំង	cʰəncʰaŋ	壁
+ថ្ងៃនេះ	tʰŋaj nih	今日
+ម្សិលមិញ	msəl məɲ	昨日
+ស្អែក	saʔʔaek	明日
+ឥឡូវនេះ	ʔajloʋ nih	今
+មាន	miən	ある
+ញុំ	ɲom	食べる
+ផឹក	pʰək	飲む
+ដេក	daek	寝る
+ភ្ញាក់	pʰɲeək	起きる
+អង្គុយ	ʔɑŋkuj	座る
+ឈរ	cʰoː	立つ
+ដើរ	daə	歩く
+ប្រើ	praə	使う
+រស់	roʔ	生きる
+ស្នាក់នៅ	snaːʔ ʔnɨʔ	住む
+ដឹង	dəŋ	知る
+គិត	kɨt	考える
+មើល	məːl	見る
+ស្តាប់	sdap	聞く
+និយាយ	nijiaj	話す
+បង្ហាញ	baŋʔhaːɲ	見せる
+បង្រៀន	bɑŋriən	教える
+សិក្សា	saʔksaː	勉強する
+ល្អ	lʔɑʔ	良い
+អាក្រក់	ʔaːkraʔk	悪い
+ធំ	tʰom	大きい
+តូច	tooc	小さい
+ពិបាក	pibaak	難しい
+ងាយស្រួល	ŋiəj srɨl	簡単な
+ក្តៅ	ktaʊ	暑い
+ត្រជាក់	trɑcʰeak	寒い
+ភ្លឺ	pʰləː	明るい
+ងងឹត	ŋɔŋɨt	暗い
+ទូលាយ	tʊːliəj	広い
+ចង្អៀត	cʰɑŋʔiət	狭い
+ក្រាស់	kraʔh	太い
+ស្ដើង	sdɨŋ	細い
+ក្រហម	krɑhaːm	赤い
+ខៀវ	kʰiəw	青い
+បៃតង	bajtɑŋ	緑の
+លឿង	luəŋ	黄色い
+ខ្មៅ	kʰmao	黒い
+ស	saʔ	白い
+សួស្ដី	suəsdei	こんにちは
+លាហើយ	lia haəj	さようなら
+អរគុណ	ʔɑːkʰɔn	ありがとう
+សុំទោស	somtoːh	ごめんなさい
+សូមស្នើរ	som snəː	おねがいします
+មិនយល់ទេ	mɨn jɨl teː	分かりません
+ហើយ	haəj	そして
+ប៉ុន្តែ	pʰonʔtae	しかし
+ដូច្នេះ	doːcʰ neih	だから
+បាទ	baːt	はい(女性)
+ចាស	cʰaː	はい(男性)
+ទេ	teː	いいえ
+និង	nɨŋ	～と
+ជប៉ុន	cʰəpʰon	日本*
+أنا	anā	私
+نحن	naḥnu	私たち
+أنتَ (男性)	anta	あなた(男性)
+أنتِ (女性)	anti	あなた(女性)
+أنتم	antum	あなたたち
+هو	huwa	彼
+هي	hiya	彼女
+هم	hum	彼等
+هذا	hādhā	これ
+ذلك	dhālika	それ
+تلك	tilka	あれ
+هنا	hunā	ここ
+هناك	hunāka	そこ
+هناك	hunāka	あそこ
+من	man	誰
+متى	matā	いつ
+أين	ayna	どこ
+ماذا	mādhā	なに
+كيف	kayfa	どうやって
+لماذا	limādhā	なぜ
+صباح	ṣabāḥ	朝
+ظهر	ẓuhr	昼
+ليل	layl	夜
+سماء	samāʾ	空
+شمس	shams	太陽
+قمر	qamar	月
+رياح	riyāḥ	風
+جبل	jabal	山
+نهر	nahr	川
+بحر	baḥr	海
+أب	ʾab	父
+أم	ʾumm	母
+أخ	ʾakh	兄弟
+أخت	ʾukht	姉妹
+عائلة	ʿāʾilah	家族
+صديق	ṣadīq	友達
+معلم	muʿallim	先生
+طالب	ṭālib	生徒
+مدينة	madīnah	町
+بيت	bayt	家
+متجر	matjar	店
+محطة	maḥaṭṭah	駅
+شركة	sharikah	会社
+مدرسة	madrasah	学校
+مصنع	maṣnaʿ	工場
+مكتبة	maktabah	図書館
+سوبر ماركت	sūbar mārkit	スーパーマーケット
+ماكدونالدز	Mākdūnāldz	マクドナルド
+قطار	qiṭār	電車
+سيارة	sayyārah	自動車
+حافلة	ḥāfilah	バス
+طريق	ṭarīq	道路
+قط	qiṭṭ	猫
+كلب	kalb	犬
+طائر	ṭāʾir	鳥
+سمك	samak	魚
+شجرة	shajarah	木
+عشب	ʿushb	草
+زهرة	zahrah	花
+رأس	raʾs	頭
+وجه	wajh	顔
+رقبة	raqabah	首
+صدر	ṣadr	胸
+بطن	baṭn	腹
+ظهر	ẓahr	背中
+ذراع	dhirāʿ	腕
+قدم	qadam	足
+قلب	qalb	心
+وقت	waqt	時間
+كلمة	kalimah	言葉
+هاتف محمول	hātif maḥmūl	スマホ
+ساعة	sāʿah	時計
+كتاب	kitāb	本
+حقيبة	ḥaqībah	鞄
+حقيبة ظهر	ḥaqībat ẓahr	リュックサック
+حذاء	ḥidhāʾ	靴
+جوارب	jawārib	靴下
+ملابس	malābis	服
+طعام	ṭaʿām	食べ物
+شراب	sharāb	飲み物
+محفظة	miḥfaẓah	財布
+مال	māl	お金
+مصباح كهربائي	miṣbāḥ kahrabāʾī	電灯
+مكتب	maktab	机
+كرسي	kursī	椅子
+باب	bāb	扉
+سرير	sarīr	ベッド
+خزانة ملابس	khizānat malābis	クローゼット
+جدار	jidār	壁
+اليوم	al-yawm	今日
+أمس	ams	昨日
+غد	ghadan	明日
+الآن	al-ān	今
+يوجد	yūjad	ある
+يأكل	yaʾkul	食べる
+يشرب	yashrab	飲む
+ينام	yanām	寝る
+يستيقظ	yastaīqiẓ	起きる
+يجلس	yajlis	座る
+يقف	yaqif	立つ
+يمشي	yamshī	歩く
+يستخدم	yastaʿmil	使う
+يعيش	yaʿīsh	生きる
+يسكن	yaskun	住む
+يعرف	yaʿrif	知る
+يفكر	yufakkir	考える
+يرى	yarā	見る
+يسمع	yasmaʿ	聞く
+يتحدث	yataḥaddath	話す
+يظهر	yuẓhir	見せる
+يعلم	yuʿallim	教える
+يدرس	yadrus	勉強する
+جيد	jayyid	良い
+سيء	sayyiʾ	悪い
+كبير	kabīr	大きい
+صغير	ṣaghīr	小さい
+صعب	ṣaʿb	難しい
+سهل	sahl	簡単な
+حار	ḥār	暑い
+بارد	bārid	寒い
+مشرق	mushriq	明るい
+مظلم	muẓlim	暗い
+واسع	wāsiʿ	広い
+ضيق	ḍayyiq	狭い
+سميك	samīk	太い
+نحيف	naḥīf	細い
+أحمر	aḥmar	赤い
+أزرق	azraq	青い
+أخضر	akhḍar	緑の
+أصفر	aṣfar	黄色い
+أسود	aswad	黒い
+أبيض	abyaḍ	白い
+مرحباً	marḥaban	こんにちは
+وداعاً	wadāʿan	さようなら
+شكراً	shukran	ありがとう
+آسف	āsif	ごめんなさい
+من فضلك	min faḍlik	おねがいします
+لا أفهم	lā afham	分かりません
+و	wa	そして
+لكن	lākin	しかし
+لذلك	li-dhālik	だから
+نعم	naʿam	はい
+لا	lā	いいえ
+و	wa	～と
+اليابان	al-Yābān	日本*
+jag		私
+vi		私たち
+du		あなた
+ni		あなたたち
+han		彼
+hon		彼女
+de		彼等
+det här		これ
+det där		それ
+det där		あれ
+här		ここ
+där		そこ
+där		あそこ
+vem		誰
+när		いつ
+var		どこ
+vad		なに
+hur		どうやって
+varför		なぜ
+morgon		朝
+dag		昼
+natt		夜
+himmel		空
+sol		太陽
+måne		月
+vind		風
+berg		山
+flod		川
+hav		海
+pappa		父
+mamma		母
+bror		兄弟
+syster		姉妹
+familj		家族
+vän		友達
+lärare		先生
+elev		生徒
+stad		町
+hem		家
+affär		店
+station		駅
+företag		会社
+skola		学校
+fabrik		工場
+bibliotek		図書館
+mataffär		スーパーマーケット
+McDonald's		マクドナルド
+tåg		電車
+bil		自動車
+buss		バス
+väg		道路
+katt		猫
+hund		犬
+fågel		鳥
+fisk		魚
+träd		木
+gräs		草
+blomma		花
+huvud		頭
+ansikte		顔
+hals		首
+bröst		胸
+mage		腹
+rygg		背中
+arm		腕
+ben		足
+hjärta		心
+tid		時間
+ord		言葉
+mobil		スマホ
+klocka		時計
+bok		本
+väska		鞄
+ryggsäck		リュックサック
+sko		靴
+strumpa		靴下
+kläder		服
+mat		食べ物
+dryck		飲み物
+plånbok		財布
+pengar		お金
+lampa		電灯
+skrivbord		机
+stol		椅子
+dörr		扉
+säng		ベッド
+garderob		クローゼット
+vägg		壁
+idag		今日
+igår		昨日
+imorgon		明日
+nu		今
+finns		ある
+äta		食べる
+dricka		飲む
+sova		寝る
+vakna		起きる
+sitta		座る
+stå		立つ
+gå		歩く
+använda		使う
+leva		生きる
+bo		住む
+veta		知る
+tänka		考える
+se		見る
+höra		聞く
+prata		話す
+visa		見せる
+lära		教える
+studera		勉強する
+bra		良い
+dålig		悪い
+stor		大きい
+liten		小さい
+svår		難しい
+lätt		簡単な
+varm		暑い
+kall		寒い
+ljus		明るい
+mörk		暗い
+bred		広い
+smal		狭い
+tjock		太い
+smal		細い
+röd		赤い
+blå		青い
+grön		緑の
+gul		黄色い
+svart		黒い
+vit		白い
+hej		こんにちは
+adjö		さようなら
+tack		ありがとう
+förlåt		ごめんなさい
+snälla		おねがいします
+jag förstår inte		分かりません
+och		そして
+men		しかし
+därför		だから
+ja		はい
+nej		いいえ
+och		～と
+Japan		日本`
